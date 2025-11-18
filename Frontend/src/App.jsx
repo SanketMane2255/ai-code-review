21023845c99a4493fde61d82872fe5a7fb469e4c
Sanket Mane <sanketmane2255@gmail.com>
2025-11-18 19:16:45 +0530

    installed prism and frontend ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
+import "prismjs/themes/prism-tomorrow.css";
+import prism from 'prismjs';
 import './App.css'
 
 function App() {
 
-  return (
+  useEffect(() => {
+    prism.highlightAll();
+  },[]);
+
+  return (   
+
     <>
+
+    <main>  
+      <div className='left'>
+        <div className='code'>
+          <pre> 
+            <code className="language-javascript">
+              {
+                `function sum() { return 1 + 1 }`
+              }
+            </code>
+          </pre>
+        </div>
+        <div className='review'>Review</div>
+      </div>
+      <div className='right'></div>
+    </main>
      
     </>
   )
